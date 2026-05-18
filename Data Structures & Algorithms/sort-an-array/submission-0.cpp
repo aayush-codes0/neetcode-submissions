1206beae541ef63480f6ef382396ed20a21c85c5
aayush-codes0 <patelaayush045@gmail.com>
2026-05-18 18:02:45 +0530

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        vector<int> ans;
+        priority_queue<int,vector<int>,greater<int>> pq;
+        for(int x: nums){
+            pq.push(x);
+        }
+        while(!pq.empty()){
+            ans.push_back(pq.top());
+            pq.pop();
+        }
+        return ans;
+    }
+};
